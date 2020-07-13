d837828c56d9f5ba0ceceb166af558f9d2e6975e
Maurício de Moura dos Santos <53577592+Maurrici@users.noreply.github.com>
2020-07-13 14:30:28 -0300

    Rotas

--- servidor.go
+++ servidor.go
@@ -1,1 +1,28 @@
-package EncurtadorUrl
+package main
+
+import (
+	"fmt"
+	"log"
+	"strings"
+	"net/http"
+
+	"github.com/Maurrici/EncurtadorUrl/url"
+)
+
+var (
+	porta int
+	urlBase string
+)
+
+func init(){
+	porta = 8888
+	urlBase = fmt.Sprintf("http://localhost:%d", porta)
+}
+
+func main(){
+	http.HandleFunc("/api/encurtar", Encurtador)
+	http.HandleFunc("/r/", Redirecionar)
+
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d",porta),nil))
+}
+
