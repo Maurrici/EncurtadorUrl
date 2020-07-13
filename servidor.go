8382466f069381761a6fe6ea03883d8e53108943
Maurício de Moura dos Santos <53577592+Maurrici@users.noreply.github.com>
2020-07-13 15:04:44 -0300

    Método Encurtar

--- servidor.go
+++ servidor.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"strings"
 	"net/http"
+	"time"
 
 	"github.com/Maurrici/EncurtadorUrl/url"
 )
@@ -20,9 +21,50 @@ func init(){
 }
 
 func main(){
-	http.HandleFunc("/api/encurtar", Encurtador)
+	http.HandleFunc("/api/encurtar", Encurtar)
 	http.HandleFunc("/r/", Redirecionar)
 
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d",porta),nil))
 }
 
+
+type Headers map[string]string
+
+func Encurtar(w http.ResponseWriter, r *http.Request){
+	if r.Method != "POST" {
+		responderCom(w, http.StatusMethodNotAllowed, Headers{"Allow": "POST"})
+
+		return
+	}
+
+	url, nova, err := url.BuscarOuCriarNovaUrl(extrairUrl(r))
+
+	if err != nil{
+		responderCom(w, http.StatusBadRequest, nil)
+
+		return
+	}
+
+	var status int
+	if nova{
+		status = http.StatusCreated
+	}else{
+		status = http.StatusOK
+	}
+
+	urlCurta := fmt.Sprintf("%s/r/%s", urlBase,url.Id)
+	responderCom(w,status,Headers{"Location": urlCurta})
+}
+
+func responderCom(w http.ResponseWriter, status int, headers Headers){
+	for k, v := range headers{
+		w.Header().Set(k,v)
+	}
+	w.WriteHeader(status)
+}
+
+func extrairUrl(r *http.Request) string{
+	url := make([]byte, r.ContentLength, r.ContentLength)
+	r.Body.Read(url)//Copiando bytes da requisição
+	return string(url)
+}
--- url/url.go
+++ url/url.go
@@ -1,1 +1,13 @@
 package url
+
+import "time"
+
+type Url struct {
+	Id string
+	Criacao time.Time
+	Destino string
+}
+
+func (url Url) BuscarOuCriarNovaUrl(urlRequest string) (Url, bool, error){
+
+}
